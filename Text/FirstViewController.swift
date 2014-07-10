1e7cbb1416e2153a7f68f185042af5984d989e42
zhaokaiyuan99 <asasas>
2014-07-10 10:38:45 +0800

    上传修改2

---
+++ Text/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  Text
+//
+//  Created by SunSet on 14-7-10.
+//  Copyright (c) 2014 zhaokaiyuan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        println("asdasdasdasd")
+        // Do any additional setup after loading the view, typically from a nib. // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
